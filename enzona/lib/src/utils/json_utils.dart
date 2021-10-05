6bd997330b622433a1d7e473ed821aca4ef8a57c
Luis <luis901101@gmail.com>
2021-10-04 20:17:38 -0400

    - JsonUtils updated with doubleToJsonString2Digits parser to match API restrictions about double value decimal digits

--- enzona/lib/src/utils/json_utils.dart
+++ enzona/lib/src/utils/json_utils.dart
@@ -1,8 +1,10 @@
 
 class JsonUtils {
   static String? toJsonString(dynamic value) => value?.toString();
-  static int? intFromJson(dynamic json) => int.tryParse(json?.toString() ?? '');
-  static double? doubleFromJson(dynamic json) => double.tryParse(json?.toString() ?? '');
-  static num? numFromJson(dynamic json) => num.tryParse(json?.toString() ?? '');
+  static String? doubleToJsonString2Digits(double? value) => value?.toStringAsFixed(2); //This toJson workaround is necessary because ENZONA API requires all the double values to be exactly 2 decimal digits
+
+  static int? intFromJson(dynamic json) => int.tryParse(json?.toString() ?? ''); //This fromJson implementations here is necessary due to a bad field type declaration on ENZONA API
+  static double? doubleFromJson(dynamic json) => double.tryParse(json?.toString() ?? ''); //This fromJson implementations here is necessary due to a bad field type declaration on ENZONA API
+  static num? numFromJson(dynamic json) => num.tryParse(json?.toString() ?? ''); //This fromJson implementations here is necessary due to a bad field type declaration on ENZONA API
   static String? stringFromJson(dynamic json) => json?.toString();
 }
